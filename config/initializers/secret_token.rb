75d656a7ce7e2ffada2e2d38ff0bb673ad348459
hanluner <hanluner@gmail.com>
2013-01-22 15:22:31 +0800

    rework third commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '0ea10cc0354473e94af1de3c993f1163e6898ed2a3dd2606575304a0afc0c6d3ca49d454e1dfb218cc9ad0e89460383856768fd22579a4389ae092eed556cba4'
+Blog::Application.config.secret_token = '633f3e91fab8b7b3db5d290eecca990aeeface11ffcbc5594314ca5a539e113826cab9595b66eeb43f38e8e13abef6a20855dfe29135fdd052dff9c7192d0ca7'
